ecb3d0f00936781d6eb1b9455eeead1f48c99ac1
Arham <topiwalaarham4@gmail.com>
2026-09-09 16:11:29 +0530

    Fix PostCSS handling of legacy @theme directive

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+const stripUnsupportedTheme = () => ({
+    postcssPlugin: 'strip-unsupported-theme',
+    Once(root) {
+        root.walkAtRules('theme', (rule) => {
+            if (rule.nodes && rule.nodes.length > 0) {
+                rule.nodes.forEach((node) => {
+                    rule.parent.insertBefore(rule, node.clone());
+                });
+            }
+            rule.remove();
+        });
     },
+});
+
+module.exports = {
+    plugins: [
+        stripUnsupportedTheme(),
+        require('tailwindcss')({}),
+        require('autoprefixer')({}),
+    ],
 };
